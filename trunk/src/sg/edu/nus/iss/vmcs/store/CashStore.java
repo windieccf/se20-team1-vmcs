8fce007f7aec46c8d16e34e8d341748b23677b55
windieccf@gmail.com <windieccf@gmail.com@2297922c-7fa7-60df-1f69-0c8c7bcbf0df>
2013-10-09 02:43:32 +0000

    project created

---
+++ trunk/src/sg/edu/nus/iss/vmcs/store/CashStore.java
@@ -0,0 +1,38 @@
+package sg.edu.nus.iss.vmcs.store;
+
+/*
+ * Copyright 2003 ISS.
+ * The contents contained in this document may not be reproduced in any
+ * form or by any means, without the written permission of ISS, other
+ * than for the purpose for which it has been supplied.
+ *
+ */
+
+/**
+ *
+ *
+ * @version 3.0 5/07/2003
+ * @author Olivo Miotto, Pang Ping Li
+ */
+
+public class CashStore extends Store {
+
+	public final static int INVALID_COIN_WEIGHT = 9999;
+
+	public CashStore() {
+	}
+	
+	public int findCashStoreIndex (Coin c) {
+		int size = getStoreSize();
+		for (int i = 0; i < size; i++) {
+			StoreItem item = (CashStoreItem) getStoreItem(i);
+			Coin current = (Coin) item.getContent();
+			if (current.getWeight() == c.getWeight())
+				return i;
+		}
+		return -1;
+	}
+
+
+}
+
